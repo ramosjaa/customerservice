8bc5a0c3651fdd7792275c6802e9e22405c8d687
Jack Ramos <ramos.jack525@yahoo.com>
2023-03-22 17:16:23 -0400

    Initial commit

---
+++ src/main/java/edu/iu/c322/customerservice/controller/GreetingController.java
@@ -0,0 +1,16 @@
+package edu.iu.c322.customerservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class GreetingController{
+
+    //Get https:localhost:8080
+
+    @GetMapping
+    public String greetings(){
+        return "Greetings!";
+    }
+}
